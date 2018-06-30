f2cec91852bacee360f0b6d5117a8d32cddada8f
Enrique <enriquemendoza_162@hotmail.com>
2018-06-30 08:57:21 -0430

    Fenet Aplicacion Movil version 1.0

---
+++ aboutService.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+
+//Valores recibidos 
+$servicio = $_POST['servicio'];
+
+
+
+//Conexión Servidor Local
+$conexion=mysql_connect("localhost","root","");
+$baseDeDatos=mysql_select_db("fenet",$conexion); 
+
+$ssqlId = mysql_query("SELECT * FROM listaservicios WHERE servicio='$servicio'");
+        	$descripcion    =  mysql_result($ssqlId,0,"descripcion");
+
+echo $descripcion;
+
+
+mysql_close($conexion);
+
+?>
+
+
